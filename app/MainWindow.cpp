64ddf8de4a2bb78fda96db6cd50faa1366631076
Kenny <kennyljh@iastate.edu>
2026-04-13 03:12:17 -0500

    completed FolderGallery

--- app/MainWindow.cpp
+++ app/MainWindow.cpp
@@ -6,8 +6,23 @@
 #include <QLineEdit>
 #include <QPushButton>
 #include <QString>
+#include <QDir>
+#include <QStringList>
+#include <QScrollArea>
+#include <QListWidget>
+#include <QEvent>
+#include <QIntValidator>
+#include <QListWidgetItem>
+#include <QDesktopServices>
+#include <QUrl>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
+
+    selectedDir = new QString();
+    folderToFiles = new QMap<QString, QList<QString>>();
+    reverse = false;
+    withinFolder = false;
+    selectedFolder = new QString();
 
     resize(1280, 720);
 
@@ -16,42 +31,305 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     setStatusBar(statusBar);
 
     centralWindow = new QWidget(this);
-
-    dirAndGoWindow = new QWidget(centralWindow);
-        dirAndGoLayout = new QHBoxLayout(dirAndGoWindow);
-            selectedDirEdt = new QLineEdit(dirAndGoWindow);
-            selectedDirEdt->setPlaceholderText("Paste directory here...");
-            confirmDirBtn = new QPushButton("Go", dirAndGoWindow);
-        dirAndGoLayout->addWidget(selectedDirEdt, 1);
-        dirAndGoLayout->addWidget(confirmDirBtn);
-
-    contentWindow = new QWidget(centralWindow);
-
-    miscWindow = new QWidget(centralWindow);
-        miscLayout = new QHBoxLayout(miscWindow);
-            leftMiscWindow = new QWidget(miscWindow);
-                leftMiscLayout = new QHBoxLayout(leftMiscWindow);
-                    backBtn = new QPushButton("Back", leftMiscWindow);
-                    refreshBtn = new QPushButton("Refresh", leftMiscWindow);
-                    alphabetSortBtn = new QPushButton("A-Z", leftMiscWindow);
-                leftMiscLayout->addWidget(backBtn);
-                leftMiscLayout->addWidget(refreshBtn);
-                leftMiscLayout->addWidget(alphabetSortBtn);
-            rightMiscWindow = new QWidget(miscWindow);
-                rightMiscLayout = new QHBoxLayout(rightMiscWindow);
-                    itemsPerRow = new QLineEdit(rightMiscWindow);
-                    incrementBtn = new QPushButton("+", rightMiscWindow);
-                    decrementBtn = new QPushButton("-", rightMiscWindow);
-                rightMiscLayout->addWidget(itemsPerRow);
-                rightMiscLayout->addWidget(incrementBtn);
-                rightMiscLayout->addWidget(decrementBtn);
-        miscLayout->addWidget(leftMiscWindow, 0, Qt::AlignLeft);
-        miscLayout->addWidget(rightMiscWindow, 0, Qt::AlignRight);
-
     centralLayout = new QVBoxLayout(centralWindow);
+
+        dirAndGoWindow = new QWidget(centralWindow);
+            dirAndGoLayout = new QHBoxLayout(dirAndGoWindow);
+                selectedDirEdt = new QLineEdit(dirAndGoWindow);
+                selectedDirEdt->setPlaceholderText("Paste directory here...");
+                confirmDirBtn = new QPushButton("Go", dirAndGoWindow);
+                connect(confirmDirBtn, &QPushButton::clicked, this, &MainWindow::goButtonClicked);
+            dirAndGoLayout->addWidget(selectedDirEdt, 1);
+            dirAndGoLayout->addWidget(confirmDirBtn);
+
+        contentWindow = new QWidget(centralWindow);
+            contentLayout = new QVBoxLayout(contentWindow);
+                contentListWidget = new QListWidget(contentWindow);
+                contentListWidget->viewport()->installEventFilter(this);
+                contentListWidget->setViewMode(QListView::IconMode);
+                contentListWidget->setSpacing(10);
+                connect(contentListWidget, &QListWidget::itemClicked, this, &MainWindow::listWidgetItemClicked);
+            contentLayout->addWidget(contentListWidget, 1);
+
+        miscWindow = new QWidget(centralWindow);
+            miscLayout = new QHBoxLayout(miscWindow);
+
+                leftMiscWindow = new QWidget(miscWindow);
+                    leftMiscLayout = new QHBoxLayout(leftMiscWindow);
+                        backBtn = new QPushButton("Back", leftMiscWindow);
+                        connect(backBtn, &QPushButton::clicked, this, &MainWindow::backButtonClicked);
+
+                        dateSortBtn = new QPushButton("Date Sort", leftMiscWindow);
+                        connect(dateSortBtn, &QPushButton::clicked, this, &MainWindow::dateSortButtonClicked);
+                    leftMiscLayout->addWidget(backBtn);
+                    leftMiscLayout->addWidget(dateSortBtn);
+
+                rightMiscWindow = new QWidget(miscWindow);
+                    rightMiscLayout = new QHBoxLayout(rightMiscWindow);
+                        itemsPerRowEdt = new QLineEdit(rightMiscWindow);
+                        QIntValidator *validator = new QIntValidator(1, 20, itemsPerRowEdt);
+                        itemsPerRowEdt->setValidator(validator);
+                        itemsPerRowEdt->setText("8");
+                        connect(itemsPerRowEdt, &QLineEdit::textChanged, this, &MainWindow::itemsPerRowChanged);
+                        connect(itemsPerRowEdt, &QLineEdit::inputRejected, this, &MainWindow::invalidRowInput);
+
+                        incrementBtn = new QPushButton("+", rightMiscWindow);
+                        connect(incrementBtn, &QPushButton::clicked, this, &MainWindow::incrementRowValueButtonClicked);
+
+                        decrementBtn = new QPushButton("-", rightMiscWindow);
+                        connect(decrementBtn, &QPushButton::clicked, this, &MainWindow::decrementRowValueButtonClicked);
+                    rightMiscLayout->addWidget(itemsPerRowEdt);
+                    rightMiscLayout->addWidget(incrementBtn);
+                    rightMiscLayout->addWidget(decrementBtn);
+            miscLayout->addWidget(leftMiscWindow, 0, Qt::AlignLeft);
+            miscLayout->addWidget(rightMiscWindow, 0, Qt::AlignRight);
+
     centralLayout->addWidget(dirAndGoWindow);
     centralLayout->addWidget(contentWindow, 1);
     centralLayout->addWidget(miscWindow);
 
     setCentralWidget(centralWindow);
+}
+
+bool MainWindow::processSelectedDir (const QString &selectedDir, QMap<QString, QList<QString>> &folderToFiles){
+
+    if (!QDir (selectedDir).exists()){
+
+        statusBar->showMessage("Directory does not exists.");
+        return false;
+    }
+
+    folderToFiles.clear();
+
+    QDir mainDir(selectedDir);
+    QStringList folderNames = mainDir.entryList(QDir::AllDirs | QDir::NoDotAndDotDot, QDir::Time);
+
+    for (const QString &folderName : folderNames){
+
+        statusBar->showMessage("Found folder: " + folderName);
+
+        QDir folderDir(mainDir.filePath(folderName));
+        QStringList files = folderDir.entryList(QDir::Files, QDir::Name);
+
+        if (files.isEmpty()) continue;
+
+        QList<QString> listOfFiles;
+
+        for (const QString &file : files){
+
+            statusBar->showMessage("Found file: " + file);
+            QDir fileDir(folderDir.filePath(file));
+            listOfFiles.append(fileDir.path());
+        }
+
+        folderToFiles.insert(folderDir.path(), listOfFiles);
+    }
+    statusBar->showMessage("Directory processed.");
+    return true;
+}
+
+void MainWindow::insertFolders (const QMap<QString, QList<QString>> &folderToFiles){
+
+    contentListWidget->clear();
+
+    for (const QString &folderPath : folderToFiles.keys()){
+
+        QList<QString> copy = folderToFiles.value(folderPath);
+        if (copy.first().isNull()) continue;
+
+        QPixmap pix(copy.first());
+        if (pix.isNull()) continue;
+
+        QListWidgetItem *folder = new QListWidgetItem(QIcon(pix), QDir (folderPath).dirName());
+        contentListWidget->addItem(folder);
+    }
+    statusBar->showMessage("Showing folders by ascending modified date.");
+}
+
+void MainWindow::reverseInsertFolders (const QMap<QString, QList<QString>> &folderToFiles){
+
+    contentListWidget->clear();
+
+    QVector<QString> keys = folderToFiles.keys();
+    std::reverse(keys.begin(), keys.end());
+
+    for (const QString &folderPath : keys){
+
+        QList<QString> copy = folderToFiles.value(folderPath);
+        if (copy.first().isNull()) continue;
+
+        QPixmap pix(copy.first());
+        if (pix.isNull()) continue;
+
+        QListWidgetItem *folder = new QListWidgetItem(QIcon(pix), QDir (folderPath).dirName());
+        contentListWidget->addItem(folder);
+    }
+    statusBar->showMessage("Showing folders by reversed modified date.");
+}
+
+void MainWindow::insertFiles(const QString &chosenFolder, const QMap<QString, QList<QString>> &folderToFiles){
+
+    contentListWidget->clear();
+    QDir folderPath(QDir (*selectedDir).filePath(chosenFolder));
+    QList<QString> copy = folderToFiles.value(folderPath.path());
+
+    for (const QString &filePath : copy){
+
+        if (filePath.isNull()) continue;
+
+        QPixmap pix(filePath);
+        if (pix.isNull()) continue;
+
+        QString fileName = QDir (filePath).dirName();
+        if (fileName.size() > 13){
+
+            QString extension = QDir (filePath).dirName().last(8);
+            fileName.truncate(5);
+            fileName.append("..." + extension);
+        }
+
+        QListWidgetItem *file = new QListWidgetItem(QIcon(pix), fileName);
+        contentListWidget->addItem(file);
+    }
+    statusBar->showMessage("Showing files for " + chosenFolder + " by ascending modified date.");
+}
+
+void MainWindow::reverseInsertFiles(const QString &chosenFolder, const QMap<QString, QList<QString>> &folderToFiles){
+
+    contentListWidget->clear();
+    QDir folderPath(QDir (*selectedDir).filePath(chosenFolder));
+
+    QVector<QString> files = folderToFiles.value(folderPath.path());
+    std::reverse(files.begin(), files.end());
+
+    for (const QString &filePath : files){
+
+        if (filePath.isNull()) continue;
+
+        QPixmap pix(filePath);
+        if (pix.isNull()) continue;
+
+        QString fileName = QDir (filePath).dirName();
+        if (fileName.size() > 13){
+            QString extension = QDir (filePath).dirName().last(8);
+            fileName.truncate(5);
+            fileName.append("..." + extension);
+        }
+
+        QListWidgetItem *file = new QListWidgetItem(QIcon(pix), fileName);
+        contentListWidget->addItem(file);
+    }
+    statusBar->showMessage("Showing files for " + chosenFolder + " by descending modified date.");
+}
+
+void MainWindow::changeItemsPerRow(int val){
+
+    int result = itemsPerRowEdt->text().toInt() + val;
+    if (result <= 0) result = 1;
+    itemsPerRowEdt->setText(QString::number(result));
+}
+
+void MainWindow::openFolderInExplorer(const QString &folderPath){
+
+    QUrl folderUrl = QUrl::fromLocalFile(folderPath);
+
+    if (!QDesktopServices::openUrl(folderUrl)){
+        statusBar->showMessage("Failed to open folder. Path: " + folderPath);
+    }
+}
+
+void MainWindow::goButtonClicked() {
+
+    if (!selectedDirEdt->text().isEmpty()) selectedDir->assign(selectedDirEdt->text());
+
+    if (!selectedDir->isEmpty() || !selectedDir->isNull()){
+
+        if (!processSelectedDir(*selectedDir, *folderToFiles)) return;
+        insertFolders(*folderToFiles);
+    }
+    else {
+        statusBar->showMessage("No directory entered.");
+    }
+}
+
+void MainWindow::incrementRowValueButtonClicked(){
+
+    changeItemsPerRow(1);
+}
+
+void MainWindow::decrementRowValueButtonClicked() {
+
+    changeItemsPerRow(-1);
+}
+
+void MainWindow::itemsPerRowChanged(){
+
+    if (!itemsPerRowEdt->hasAcceptableInput()) return;
+    statusBar->clearMessage();
+
+    int width = contentListWidget->viewport()->width();
+    int spacing = contentListWidget->spacing();
+    int iconWidth = (width - (itemsPerRowEdt->text().toInt() * 2) * spacing) / itemsPerRowEdt->text().toInt();
+
+    contentListWidget->setIconSize(QSize(iconWidth, iconWidth * 1.414));
+}
+
+void MainWindow::invalidRowInput(){
+
+    statusBar->showMessage("Row value between 0-20 only.");
+}
+
+void MainWindow::dateSortButtonClicked(){
+
+    reverse = !reverse;
+    if (!withinFolder && reverse){
+        reverseInsertFolders(*folderToFiles);
+    }
+    else if (!withinFolder && !reverse) {
+        insertFolders(*folderToFiles);
+    }
+    else if (withinFolder && reverse){
+        reverseInsertFiles(*selectedFolder, *folderToFiles);
+    }
+    else if (withinFolder && !reverse){
+        insertFiles(*selectedFolder, *folderToFiles);
+    }
+}
+
+void MainWindow::listWidgetItemClicked(QListWidgetItem *item){
+
+    if (item && !withinFolder) {
+
+        *selectedFolder = item->text();
+
+        reverse = true;
+        insertFiles(item->text(), *folderToFiles);
+        withinFolder = !withinFolder;
+    }
+    else if (item && withinFolder){
+        openFolderInExplorer(QDir (*selectedDir).filePath(*selectedFolder));
+    }
+}
+
+void MainWindow::backButtonClicked(){
+
+    if (withinFolder){
+        reverse = true;
+        insertFolders(*folderToFiles);
+        withinFolder = !withinFolder;
+    }
+}
+
+bool MainWindow::eventFilter(QObject *watched, QEvent *event) {
+
+    if (watched == contentListWidget->viewport() &&
+        event->type() == QEvent::Resize) {
+
+        int width = contentListWidget->viewport()->width();
+        int spacing = contentListWidget->spacing();
+        int iconWidth = (width - (itemsPerRowEdt->text().toInt() * 2) * spacing) / itemsPerRowEdt->text().toInt();
+
+        contentListWidget->setIconSize(QSize(iconWidth, iconWidth * 1.414));
+    }
+    return QMainWindow::eventFilter(watched, event);
 }
--- app/MainWindow.h
+++ app/MainWindow.h
@@ -6,7 +6,7 @@
 #include <QString>
 #include <QMap>
 #include <QList>
-#include <QDockWidget>
+#include <QEvent>
 
 class QStatusBar;
 class QVBoxLayout;
@@ -14,14 +14,25 @@ class QHBoxLayout;
 class QPushButton;
 class QLineEdit;
 class QDockWidget;
+class QScrollArea;
+class QListWidget;
+class QListWidgetItem;
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
     public:
-    explicit MainWindow(QWidget *parent = 0);
+        explicit MainWindow(QWidget *parent = 0);
 
     private:
+        // preferences
         QString *selectedDir;
+        bool reverse;
+        bool withinFolder;
+        QString *selectedFolder;
+
+        /**
+         * @brief folderToFiles - Stores mappings of folder absolute file paths their included files' names
+         */
         QMap<QString, QList<QString>> *folderToFiles;
 
         QStatusBar *statusBar;
@@ -35,14 +46,54 @@ class MainWindow : public QMainWindow {
         QHBoxLayout *dirAndGoLayout;
 
         QWidget *contentWindow;
+        QListWidget *contentListWidget;
+        QVBoxLayout *contentLayout;
 
         QWidget *miscWindow, *leftMiscWindow, *rightMiscWindow;
         QHBoxLayout *miscLayout, *leftMiscLayout, *rightMiscLayout;
-        QPushButton *backBtn, *refreshBtn, *alphabetSortBtn,
-                        *incrementBtn, *decrementBtn;
-        QLineEdit *itemsPerRow;
+        QPushButton *backBtn, *dateSortBtn, *incrementBtn,
+                        *decrementBtn;
+        QLineEdit *itemsPerRowEdt;
+
+        /**
+         * @brief processSelectedDir - Takes desired directory to retrieve all folders and associated files
+         * @param selectedDir - desired directory
+         * @param folderToFiles - mappings of folder absolute paths to files' names
+         */
+        bool processSelectedDir (const QString &selectedDir, QMap<QString, QList<QString>> &folderToFiles);
+
+        /**
+         * @brief insertFolders - inserts folders' first picture into listWidget
+         * @param folderToFiles - mappings of folder absolute paths to files' names
+         */
+        void insertFolders (const QMap<QString, QList<QString>> &folderToFiles);
+
+        void reverseInsertFolders (const QMap<QString, QList<QString>> &folderToFiles);
+
+        /**
+         * @brief insertFiles - inserts image files into list widget
+         * @param chosenFolder - desired folder
+         * @param folderToFiles - mappings of folder absolute paths to files' names
+         */
+        void insertFiles(const QString &chosenFolder, const QMap<QString, QList<QString>> &folderToFiles);
+
+        void reverseInsertFiles(const QString &chosenFolder, const QMap<QString, QList<QString>> &folderToFiles);
+
+        void changeItemsPerRow(int val);
+
+        void openFolderInExplorer(const QString &folderPath);
+
+        bool eventFilter(QObject *watched, QEvent *event) override;
     signals:
-    public slots:
+    private slots:
+        void goButtonClicked();
+        void incrementRowValueButtonClicked();
+        void decrementRowValueButtonClicked();
+        void itemsPerRowChanged();
+        void invalidRowInput();
+        void dateSortButtonClicked();
+        void listWidgetItemClicked(QListWidgetItem *item);
+        void backButtonClicked();
 };
 
 #endif // MAINWINDOW_H
