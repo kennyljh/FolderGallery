00c17d4572b57cf4379d0bd38c234c0af908ad2e
Kenny <kennyljh23@gmail.com>
2026-05-15 13:21:21 -0500

    added empty folder checking in iomanager. bug in commit 725a6e possibly caused by currentCards not updated correctly

--- FolderGallery/iomanager.cpp
+++ FolderGallery/iomanager.cpp
@@ -34,20 +34,26 @@ void IOManager::processDirAsync(const QString &absolutePath){
             QDir folderDir(folder.absoluteFilePath());
             QFileInfoList fileList = folderDir.entryInfoList(QDir::Files,
                                                              QDir::Name);
+
+            if (fileList.isEmpty()) {
+                qDebug() << "Empty folder skipped: " + folder.baseName();
+                continue;
+            }
+
             folderBundle bundle;
             bundle.folderInfo = folder;
             bundle.filesInfos = fileList;
 
             namesToFolderBundles.insert(folder.baseName(), bundle);
 
-            //qDebug() << "Found folder: " + folder.baseName() + " with " +
+            qDebug() << "Found folder: " + folder.baseName() + " with " +
                         QString::number(fileList.size()) + " files";
         }
 
-        QMetaObject::invokeMethod(this, [this, folderList, namesToFolderBundles](){
+        QMetaObject::invokeMethod(this, [this, namesToFolderBundles](){
 
             dirProcessDone(namesToFolderBundles);
-            emit IOSuccess("Found " + QString::number(folderList.length()) + " folders.");
+            emit IOSuccess("Found " + QString::number(namesToFolderBundles.keys().size()) + " folders.");
         }, Qt::QueuedConnection);
     });
 }
